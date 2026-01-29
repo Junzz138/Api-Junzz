720de4bbc9b6436331b39e7145c8cdd53ac2b92e
Junzz138 <eppestok92@gmail.com>
2026-01-29 15:12:30 +0700

    Update tohitam.js

--- api/tohitam.js
+++ api/tohitam.js
@@ -8,7 +8,7 @@ module.exports = {
     if (!apikey || !global.apikey.includes(apikey)) return res.json({ status: false, error: "Apikey invalid" });
     if (!url) return res.json({ status: false, error: "Url is required" });
     try {
-      const ap = await fetchJson(`https://api.platform.web.id/hitam?imageUrl=${url}`)
+      const ap = await fetchJson(`https://api-faa.my.id/faa/tohitam?url=${url}`)
       res.status(200).json({ status: true, result: ap.image.url });
     } catch (e) {
       res.status(500).json({ status: false, error: e.message });
